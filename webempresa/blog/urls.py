9625674d2dde16e66abc04bd76569a68379ca38b
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-05-24 00:53:26 -0500

    correcciones en la pagina de categoria de blogs

--- webempresa/blog/urls.py
+++ webempresa/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.blog, name="blog"),
+    path('', views.blog, name="blog"),
+    path('/category/<int:category_id>/', views.category, name="category"),
 ]
