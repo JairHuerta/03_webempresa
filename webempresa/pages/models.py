6370e766fdeea189846c036b2e5331093f0a46e4
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-05-24 14:53:37 -0500

    tags personalizados

--- webempresa/pages/models.py
+++ webempresa/pages/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Titulo", max_length=200)
     content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
 
--- webempresa/pages/views.py
+++ webempresa/pages/views.py
@@ -4,4 +4,4 @@ from .models import Page
 # Create your views here.
 def page(request, page_id, page_slug):
     page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/sample.html', {'page':page})
+    return render(request, "pages/sample.html",{'page':page})
